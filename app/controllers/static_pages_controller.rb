f42981bbf5be57913ce7879fb30acf3223789523
kenjikatooo <k.524.10nis@gmail.com>
2018-06-24 09:32:45 +0900

    文字を追加

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,7 @@
+class StaticPagesController < ApplicationController
+  def login
+  end
+
+  def home
+  end
+end
---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get login" do
+    get static_pages_login_url
+    assert_response :success
+  end
+
+end
